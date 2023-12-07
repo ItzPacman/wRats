8788e1bfe1bae0d836aad9b55ad3b4fa30f8e447
pacman <0xpacman.eth@gmail.com>
2023-12-07 03:41:12 -0800

    fixed errors

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+  solidity: "0.8.16",
+
+  networks: {
+
+    fuji: {
+      url: process.env.AVAX_RPC_URL ,
+      accounts: [process.env.PRIVATE_KEY!] ,
+      gasPrice: 225000000000,
+      chainId: 43113,
+
+    }
+}
+
+}
 
 export default config;
--- backend/scripts/deploy.ts
+++ backend/scripts/deploy.ts
@@ -1,27 +1,27 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Deploy SafeMath first
+  const SafeMath = await ethers.getContractFactory("SafeMath");
+  const safeMath = await SafeMath.deploy();
+  await safeMath.waitForDeployment();
 
-  const lockedAmount = ethers.parseEther("0.001");
+  console.log(`SafeMath deployed at: ${safeMath.target}`);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  //Deploy the main contract with library addresses
+  const wRats = await ethers.getContractFactory("wRats");
+  const wrats = await wRats.deploy();
+  await wrats.waitForDeployment();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`wRats deployed at: ${wrats.target}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+//   SafeMath deployed at: 0xB1EA59521a88405D313d412f3f3EFCF4a329f2dc
+// wRats deployed at: 0xf47fd57b08263Db11D9a039268b3b2975D0c3A36
